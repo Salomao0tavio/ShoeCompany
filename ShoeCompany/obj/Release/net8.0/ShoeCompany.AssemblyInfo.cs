fb43cb8059dc4d428124c4a0c3f5294f336346e9
gitName <a@b.c>
2024-03-04 13:17:20 -0300

    added setup.sh

--- ShoeCompany/obj/Release/net8.0/ShoeCompany.AssemblyInfo.cs
+++ ShoeCompany/obj/Release/net8.0/ShoeCompany.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoeCompany")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fff5d99c9758f7a79566eded2bc71db247340c47")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoeCompany")]
